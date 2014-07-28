6315d5a1ec25037d3795cdac2bc1b907ce71d59c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-07-29 02:04:40 +0530

    Rewrote the code to log ping time
    Monitors average time in ping

--- monitor.sh
+++ monitor.sh
@@ -1,23 +1,39 @@
 ######################################
-# Author  : Praveen Kumar Pendyala
-# Created : 28 June 2014
-# License : GPL v2
+# Author   : Praveen Kumar Pendyala
+# Created  : 28 June 2014
+# Modified : 29 June 2014
+# License  : GPL v2
 ######################################
-echo "Ping failures to open dns (208.67.222.222)" >pinglog
-hit=" ---- hit"
+
+hit=" ---- hit   ----- "
 fail=" ---- fail"
 
+echo "Ping failures to open dns (208.67.222.222)" >log
+
+# Initial messages
 echo -e "\nNetwork monitoring started. Check logs in pinglog file."
 echo -e "Ping running...."
-echo -e "\n\n ========= Logging started ========== \n" >>pinglog
+echo -e "\n\n ========= Logging started ========== \n" >>log
+
+# Continious monitoring starts
 while true; do
-  if ! ping -c 1 -q 208.67.222.222 >/dev/null;
-  then
-		time=$(date)
-    echo $time$fail  >> pinglog
-  else
-    time=$(date)
-    echo $time$hit  >> pinglog
-  fi
-  sleep 57
+
+	# Attempt ping and extract times to $result.
+	# Ping errors, if any, are appended to log
+	result=$(ping -c 1 208.67.222.222 2>>log | tail -1 | awk '{print $4}')
+
+	# Check if ping actually returned times
+	if [ "$result" != "" ]; then
+		# extract avg. time
+		avg=$(echo $result | cut -d '/' -f 2)       
+		echo $(date)$hit$avg" ms"  >> log
+
+	# Handle errors
+	else
+		echo $(date)$fail  >> log
+	fi
+
+	# wait for a 57 secs
+	sleep 57
+
 done
