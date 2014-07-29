614ae861757f7061f83c2438d060001b747a4253
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-07-29 20:44:12 +0530

    Added support for ISP login

--- monitor.sh
+++ monitor.sh
@@ -11,10 +11,14 @@
 # Ping destination. Default: 208.67.222.222
 host=208.67.222.222
 
-# Wait time (seconds) before next ping. Default: 57
-wait=1
+# Does your ISP require login and it logs you after sometime? Default=0
+# If set to true, update values in ISPs with logging below.
+isp_login=0
 
-# Count in each ping (ping -c C). Default: 2
+# Wait time (seconds) before next ping. Default: 57
+wait=57
+
+# Count in each ping (ping -c c). Default: 2
 # Average of these many pings will be logged
 c=2  #Pings per test
 
@@ -24,14 +28,46 @@ hit_msg=" ---- hit   ----- "
 # Message appended to timestamp when it's a fail
 fail_msg=" ---- fail"
 
+
+### For ISPs with loging ###
+
+# For ISPs that require login, we may get continuous fails when the user has 
+# been logged out automatically after certain time. In such cases, we try to 
+# login again after set number of successive fails and then continue with the 
+# pinging test.
+
+# Successive fails after which we attempt a ISP login. Default=10
+fail_limit=10
+
+# ISP login url
+isp_url="http://myisp.com/login"
+
+# ISP username
+isp_usr="username"
+
+# ISP password
+isp_pwd="password"
+
+# ISP other post data, if any. Default:""
+isp_mre_post=""
+
+# ISP cookie info
+isp_cookies="--save-cookies cookies.txt --keep-session-cookies"
+
 ############  END OF PARAMS ##############
+
+# Number of successive fails.
+# Once this = $fail_limit we attempt an ISP login (if enabled).
+scs_fails=0
+
+# Build ISP wget url
 
 
 echo "Ping monitoring with " $host >log
 
 # Initial messages
 echo -e "\nNetwork monitoring started. Check log file."
-echo -e "Ping running...."
+echo "Ping running...."
 echo -e "\n\n ========= Logging started ========== \n" >>log
 
 # Continious monitoring starts
@@ -45,10 +81,19 @@ while true; do
 	if [ "$result" != "" ]; then
 		avg=$(echo $result | cut -d '/' -f 2)       
 		echo $(date) $hit_msg $avg" ms"  >> log
+		scs_fails=0
 
 	# Handle errors
 	else
 		echo $(date) $fail_msg  >> log
+		scs_fails=$(($scs_fails + 1))
+	fi
+
+	# Check if ISP login condition reached
+	if [ $isp_login -eq 1 ] && [ $scs_fails -eq $fail_limit ]; then
+		echo -e $(date) "Performing ISP login\n" >> log
+		postdata="username=$isp_usr&password=$isp_pwd"
+		wget $isp_cookies --post-data="$postdata" "$isp_url" 2>> log
 	fi
 
 	# wait before next run
